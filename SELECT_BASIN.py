7017cde0d140321b13a4d7c5a6be1d34cc86b462
mbaldacchino-siena <mathys.baldacchino@sienacapitalgroup.com>
2026-03-13 11:59:51 +0100

    feat: update basin selection according to other changes

--- SELECT_BASIN.py
+++ SELECT_BASIN.py
@@ -1,120 +1,60 @@
 # -*- coding: utf-8 -*-
 """
-@author: Nadia Bloemendaal, nadia.bloemendaal@vu.nl
-
-For more information, please see 
-Bloemendaal, N., Haigh, I.D., de Moel, H. et al. 
-Generation of a global synthetic tropical cyclone hazard dataset using STORM. 
-Sci Data 7, 40 (2020). https://doi.org/10.1038/s41597-020-0381-2
-
-This is the STORM module for simulation of genesis month, frequency, and basin boundaries
-
-Copyright (C) 2020 Nadia Bloemendaal. All versions released under the GNU General Public License v3.0
+STORM module for simulation of genesis month, frequency, and basin boundaries.
+SIENA extension: optional ENSO-phase-aware sampling with pooled fallback.
 """
 import numpy as np
 import random
 import os
 import sys
+from siena_utils import normalize_phase, phase_code
+
 dir_path=os.path.dirname(os.path.realpath(sys.argv[0]))
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-#Basin indices: 
-# 0 = EP = Eastern Pacific
-# 1 = NA = North Atlantic
-# 2 = NI = North Indian
-# 3 = SI = South Indian
-# 4 = SP = South Pacific
-# 5 = WP = Western Pacific
 
-def Genesis_month(idx,storms):
-    """
-    Sample the genesis months for every TC
-    Parameters
-    ----------
-    idx : basin index (0=EP 1=NA 2=NI 3=SI 4=SP 5=WP).
-    storms : number of TCs.
-
-    Returns
-    -------
-    monthall : list of all genesis months.
-
-    """
+def Genesis_month(idx, storms, phase=None):
+    phase = normalize_phase(phase)
+    if phase is not None and os.path.exists(os.path.join(__location__, 'GENESIS_MONTHS_PHASE.npy')):
+        monthlist = np.load(os.path.join(__location__, 'GENESIS_MONTHS_PHASE.npy'), allow_pickle=True).item()
+        choices = monthlist[idx].get(phase, [])
+        if len(choices) > 0:
+            return [int(np.random.choice(choices)) for _ in range(storms)]
     monthlist=np.load(os.path.join(__location__,'GENESIS_MONTHS.npy'),allow_pickle=True).item()
-    
-    monthall=[]
-    for i in range(0,storms):
-        monthall.append(np.random.choice(monthlist[idx]))
-        
-            
-    
-    return monthall
+    return [int(np.random.choice(monthlist[idx])) for _ in range(storms)]
 
 
-    
-def Storms(idx): 
-    """
-    Sample the number of TC formations in a given year
-
-    Parameters
-    ----------
-    idx : basin index (0=EP 1=NA 2=NI 3=SI 4=SP 5=WP).
-
-    Returns
-    -------
-    s : number of storms.
-
-    """
+def Storms(idx, phase=None):
+    phase = normalize_phase(phase)
+    if phase is not None and os.path.exists(os.path.join(__location__, 'POISSON_GENESIS_PARAMETERS_PHASE.npy')):
+        mu_dict = np.load(os.path.join(__location__, 'POISSON_GENESIS_PARAMETERS_PHASE.npy'), allow_pickle=True).item()
+        mu = mu_dict[idx].get(phase_code(phase), 0)
+        if mu > 0:
+            poisson=np.random.poisson(mu,10000)
+            return int(random.choice(poisson))
     mu_list=np.loadtxt(os.path.join(__location__,'POISSON_GENESIS_PARAMETERS.txt'))
-    #mu_list has the shape [EP,NA,NI,SI,SP,WP]
-    
-    mu=mu_list[idx]
-
+    mu=float(mu_list[idx])
     poisson=np.random.poisson(mu,10000)
-    s=random.choice(poisson)
-    return s
+    return int(random.choice(poisson))
 
-def Basins_WMO(basin):
-    """
-    Basin definitions
 
-    Parameters
-    ----------
-    basin : basin.
-
-    Returns
-    -------
-    s : number of storms.
-    month : list of genesis months.
-    lat0 : lower left corner latitude.
-    lat1 : upper right corner latitude.
-    lon0 : lower left corner longitude.
-    lon1 : upper right corner longitude.
-
-    """
-    #We follow the basin definitions from the IBTrACS dataset, but with lat boundaries set at 60 N/S
-    #The ENP/AO border will be defined in the algorithm later. 
-    
+def Basins_WMO(basin, phase=None):
     basins=['EP','NA','NI','SI','SP','WP']
     basin_name = dict(zip(basins,[0,1,2,3,4,5]))
     idx=basin_name[basin]
-    
-    s=Storms(idx)
-    
-    month=Genesis_month(idx,s)
-  
-    if idx==0: #Eastern Pacific
+    s=Storms(idx, phase=phase)
+    month=Genesis_month(idx,s, phase=phase)
+
+    if idx==0:
         lat0,lat1,lon0,lon1=5,60,180,285
-    if idx==1: #North Atlantic
+    if idx==1:
         lat0,lat1,lon0,lon1=5,60,255,359
-    if idx==2: #North Indian
+    if idx==2:
         lat0,lat1,lon0,lon1=5,60,30,100
-    if idx==3: #South Indian
+    if idx==3:
         lat0,lat1,lon0,lon1=-60,-5,10,135
-    if idx==4: #South Pacific
+    if idx==4:
         lat0,lat1,lon0,lon1=-60,-5,135,240
-    if idx==5: #Western Pacific
+    if idx==5:
         lat0,lat1,lon0,lon1=5,60,100,180
-        
-    return s,month,lat0,lat1,lon0,lon1 
-
-
+    return s,month,lat0,lat1,lon0,lon1
